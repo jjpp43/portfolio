45cd0c26180a62023147a58486e7e93ed653c617
Jazzy <jparkdev63@gmail.com>
2024-10-27 18:44:35 -0400

    Update mobile navbar ui

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -12,6 +12,8 @@ import {
   Button,
 } from "@nextui-org/react";
 import { useScroll } from "./components/scrollContext";
+import menuIcon from "../public/menu.svg";
+import { MenuIcon } from "./components/menuIcon";
 
 export default function NavigationBar() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
@@ -20,28 +22,16 @@ export default function NavigationBar() {
   const scrollToSection = (ref: React.RefObject<HTMLDivElement>) => {
     ref.current?.scrollIntoView({ behavior: "smooth" });
   };
-  const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
-  ];
+  const menuItems = ["About", "Projects", "Contact"];
 
   return (
     <Navbar
-      className="py-6 z-50 fixed sm:px-20 md:px-24 lg:px-28 xl:px-32 bg-transparent"
+      className="py-6 z-50 sm::fixed md::fixed lg:fixed xl:fixed sm:px-20 md:px-24 lg:px-28 xl:px-32 bg-transparent"
       onMenuOpenChange={setIsMenuOpen}
+      disableAnimation
     >
       <NavbarContent className="sm:hidden block px-8">
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-        />
+        <NavbarMenuToggle icon={MenuIcon} srOnlyText=" " />
       </NavbarContent>
       <NavbarContent
         className="hidden sm:flex gap-12 underline underline-offset-8 mix-blend-difference"
@@ -87,9 +77,7 @@ export default function NavigationBar() {
               className="w-full"
               href="#"
               size="lg"
-            >
-              {item}
-            </Link>
+            ></Link>
           </NavbarMenuItem>
         ))}
       </NavbarMenu>
