665b9571b9db03ae9ecab4dae9c6fcbcc83cd4f7
Jazzy <jparkdev63@gmail.com>
2024-11-12 13:31:26 -0500

    color change of bouncing arrow svg

--- app/page.tsx
+++ app/page.tsx
@@ -24,14 +24,6 @@ export default function Home() {
               style={{ display: "inline-block" }}
             />
           </div>
-          {/* <div className="flex flex-col items-center">
-            <div className="">Meme of the day</div>
-            <div className="flex flex-col justify-center items-center border border-foreground w-52 h-52 rounded-lg overflow-hidden hover:backdrop-invert">
-              <div className={`${pixelify.className} text-9xl`}>?</div>
-              <div className={`${pixelify.className}`}>- CLICK -</div>
-              {/* <Image alt="" src="/meme1.png" height={240} width={240} /> }
-            </div>
-          </div> */}
         </div>
         <div className="flex flex-row justify-end">
           <div className="font-normal text-foreground text-[10vw]">
@@ -46,21 +38,18 @@ export default function Home() {
             ideas into <strong className="text-foreground">reality.</strong>
           </div>
           {/* Bouncing Arrow */}
-          <div
-            id="arrow"
-            className="flex flex-col p-4 py-8 animate-bounce hover:scale-120"
-          >
+          <div id="arrow" className="flex flex-col p-4 py-8 animate-bounce ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="30"
               height="15"
               viewBox="0 0 26 15"
-              fill="none"
+              fill="white"
             >
               <path
                 d="M12.8768 14.2772C12.1755 14.2858 11.4995 14.0161 10.9968 13.5272L1.45675 3.98715C1.15222 3.65038 0.988758 3.20945 1.00016 2.75554C1.01157 2.30164 1.19697 1.86948 1.51802 1.54842C1.83908 1.22736 2.27125 1.04196 2.72515 1.03056C3.17905 1.01915 3.61998 1.18262 3.95675 1.48715L12.6968 10.2272C12.7201 10.2513 12.748 10.2706 12.779 10.2837C12.8099 10.2969 12.8431 10.3037 12.8768 10.3037C12.9104 10.3037 12.9436 10.2969 12.9745 10.2837C13.0055 10.2706 13.0334 10.2513 13.0568 10.2272L21.7968 1.45715C22.1335 1.15262 22.5745 0.989155 23.0284 1.00056C23.4823 1.01196 23.9144 1.19736 24.2355 1.51842C24.5565 1.83948 24.7419 2.27164 24.7533 2.72555C24.7648 3.17945 24.6013 3.62038 24.2968 3.95715L14.7568 13.5272C14.254 14.0161 13.578 14.2858 12.8768 14.2772Z"
-                fill="foreground"
-                stroke="foreground"
+                fill="white"
+                stroke="white"
               />
             </svg>
             <svg
@@ -68,12 +57,12 @@ export default function Home() {
               width="30"
               height="15"
               viewBox="0 0 26 15"
-              fill="none"
+              fill="white"
             >
               <path
                 d="M12.8768 14.2772C12.1755 14.2858 11.4995 14.0161 10.9968 13.5272L1.45675 3.98715C1.15222 3.65038 0.988758 3.20945 1.00016 2.75554C1.01157 2.30164 1.19697 1.86948 1.51802 1.54842C1.83908 1.22736 2.27125 1.04196 2.72515 1.03056C3.17905 1.01915 3.61998 1.18262 3.95675 1.48715L12.6968 10.2272C12.7201 10.2513 12.748 10.2706 12.779 10.2837C12.8099 10.2969 12.8431 10.3037 12.8768 10.3037C12.9104 10.3037 12.9436 10.2969 12.9745 10.2837C13.0055 10.2706 13.0334 10.2513 13.0568 10.2272L21.7968 1.45715C22.1335 1.15262 22.5745 0.989155 23.0284 1.00056C23.4823 1.01196 23.9144 1.19736 24.2355 1.51842C24.5565 1.83948 24.7419 2.27164 24.7533 2.72555C24.7648 3.17945 24.6013 3.62038 24.2968 3.95715L14.7568 13.5272C14.254 14.0161 13.578 14.2858 12.8768 14.2772Z"
-                fill="foreground"
-                stroke="foreground"
+                fill="white"
+                stroke="white"
               />
             </svg>
           </div>
@@ -93,7 +82,7 @@ export default function Home() {
                   src="/profile.png"
                   width="300"
                   height="360"
-                  loading="lazy"
+                  priority
                 />
               </div>
               <div className="flex flex-row items-center">
