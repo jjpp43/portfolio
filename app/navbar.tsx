356a637fe1d8b819872aa63f2a8541648bc9b26f
Jazzy <jparkdev63@gmail.com>
2024-11-11 20:40:14 -0500

    Update animation on navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -10,12 +10,16 @@ import {
   NavbarMenuItem,
   Link,
   Button,
+  Divider,
 } from "@nextui-org/react";
 import { useScroll } from "./components/scrollContext";
 import { MenuIcon } from "./components/menuIcon";
 
 export default function NavigationBar() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isMenuOpen, setIsMenuOpen] = React.useReducer(
+    (current) => !current,
+    false
+  );
   const { aboutRef, projectsRef } = useScroll();
 
   const scrollToSection = (ref: React.RefObject<HTMLDivElement>) => {
@@ -25,22 +29,30 @@ export default function NavigationBar() {
 
   return (
     <Navbar
-      className="py-6 z-40 sm:fixed md:fixed lg:fixed xl:fixed sm:px-20 md:px-24 lg:px-28 xl:px-32 bg-black sm:bg-transparent md:bg-transparent lg:bg-transparent xl:bg-transparent"
+      isMenuOpen={isMenuOpen}
       onMenuOpenChange={setIsMenuOpen}
+      className="py-6 z-40 sm:fixed md:fixed lg:fixed xl:fixed sm:px-20 md:px-24 lg:px-28 xl:px-32 bg-black sm:bg-transparent md:bg-transparent lg:bg-transparent xl:bg-transparent"
     >
       <NavbarContent className="sm:hidden block px-8">
-        <NavbarMenuToggle srOnlyText=" " icon={MenuIcon} />
+        <NavbarMenuToggle
+          srOnlyText=" "
+          icon={MenuIcon}
+          onClick={setIsMenuOpen}
+        />
       </NavbarContent>
-      <NavbarContent
-        className="hidden sm:flex gap-12 underline underline-offset-8 "
-        justify="center"
-      >
+      <NavbarContent className="hidden sm:flex gap-12" justify="center">
         <NavbarItem>
           <Link
-            className="z-20focus:outline-none cursor-pointer"
+            className="z-20 focus:outline-none cursor-pointer"
             onClick={() => scrollToSection(aboutRef)}
           >
-            <div className="">About</div>
+            <div className="relative flex flex-col group overflow-hidden">
+              <div className="z-10 p-1 group-hover:px-2 transition-all duration-300 group-hover:text-black">
+                About
+              </div>
+              <Divider />
+              <div className="absolute inset-0 bg-gray-200 transition-transform duration-500 ease-in-out transform translate-y-full group-hover:translate-y-0"></div>
+            </div>
           </Link>
         </NavbarItem>
         <NavbarItem isActive>
@@ -48,7 +60,13 @@ export default function NavigationBar() {
             className="focus:outline-none cursor-pointer"
             onClick={() => scrollToSection(projectsRef)}
           >
-            Projects
+            <div className="relative flex flex-col group overflow-hidden">
+              <div className="z-10 p-1 group-hover:px-2 transition-all duration-300 group-hover:text-black">
+                Projects
+              </div>
+              <Divider />
+              <div className="absolute inset-0 bg-gray-200 transition-transform duration-500 ease-in-out transform translate-y-full group-hover:translate-y-0"></div>
+            </div>
           </Link>
         </NavbarItem>
         <NavbarItem isActive>
@@ -56,25 +74,26 @@ export default function NavigationBar() {
             className="focus:outline-none cursor-pointer"
             onClick={() => scrollToSection(projectsRef)}
           >
-            <div className="">Contact</div>
+            <div className="relative flex flex-col group overflow-hidden">
+              <div className="z-10 p-1 group-hover:px-2 transition-all duration-300 group-hover:text-black">
+                Contact
+              </div>
+              <Divider />
+              <div className="absolute inset-0 bg-gray-200 transition-transform duration-500 ease-in-out transform translate-y-full group-hover:translate-y-0"></div>
+            </div>
           </Link>
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarMenu data-menu-open={isMenuOpen}>
+      <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
             <Link
-              color={
-                index === 2
-                  ? "primary"
-                  : index === menuItems.length - 1
-                  ? "danger"
-                  : "foreground"
-              }
+              color="foreground"
               className="w-full"
-              href="#"
+              href={"#"}
               size="lg"
+              onClick={setIsMenuOpen}
             >
               {item}
             </Link>
